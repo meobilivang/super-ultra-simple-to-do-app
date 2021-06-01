54759d972e2a4aa523e36283ab61b090c93ba87b
Julian P. Nguyen <pnguyen4@conncoll.edu>
2021-06-01 20:27:43 +0700

    build: edit build + test script

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,7 +1,13 @@
 echo '-----------------------------------------------'
+echo '						     '
+echo '						     '
 echo 'I. Phase One'
-echo 'Building Project:'
+echo '						     '
+echo '						     '
+echo '==============Building Project================='
+echo '						     '
 echo '-----------------------------------------------'
+echo '						     '
 echo 'Installing Packages via `npm`...'
 set -x
 npm install
--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,4 +1,9 @@
-echo 'Running Test cases:'
+echo '-----------------------------------------------'
+echo '						     '
+echo '						     '
+echo 'II. Phase Two'
+echo '						     '
+echo '=============Running Test cases================'
 echo '-----------------------------------------------'
 set -x
 npm test
