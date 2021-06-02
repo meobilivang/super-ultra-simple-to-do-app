82e08d670441a1c6729e9bc913072ebf66a36dce
Julian P. Nguyen <pnguyen4@conncoll.edu>
2021-06-02 14:37:01 +0700

    build: add remove img

---
+++ jenkins/scripts/remove-img.sh
@@ -0,0 +1,7 @@
+echo '-------------------------------------'
+echo '                                     '
+echo 'Remove Built Image from Local Machine'
+echo '                                     '
+echo '                                     '
+echo '-------------------------------------'
+docker rmi $registry:1.$BUILD_NUMBER
