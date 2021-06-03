319e796671976d9031bd56b8617f98ff239c2a36
Julian P. Nguyen <pnguyen4@conncoll.edu>
2021-06-03 22:23:23 +0700

    build: bash scripts edit

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -6,9 +6,18 @@ echo '=============DEPLOYING TO-DO APPLICATION TO REMOTE HOST============='
 echo '						     			  '
 echo '						     			  '
 echo '--------------------------------------------------------------------'
-
+echo '						     '
 #Run Ansible Playbooks
 ansible-playbook -i $(pwd)/ansible/hosts $(pwd)/ansible/site.yml
 
 #Clean `vm-host`
-rm -f  $(pwd)/* 
+rm -rf  $(pwd)/* 
+
+echo '---------------------------------------------'
+echo '						     			  '
+echo '						     			  '
+echo '						     			  '
+echo '=============DEPLOYMENT COMPLETED============='
+echo '						     			  '
+echo '						     			  '
+echo '---------------------------------------------'
