a2aa7cd673785aa8d967f088010f4c883052d328
Julian P. Nguyen <pnguyen4@conncoll.edu>
2021-06-03 22:05:46 +0700

    build: edit Ansible syntax

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -6,8 +6,9 @@ echo '=============DEPLOYING TO-DO APPLICATION TO REMOTE HOST============='
 echo '						     			  '
 echo '						     			  '
 echo '--------------------------------------------------------------------'
+
 #Run Ansible Playbooks
 ansible-playbook -i $(pwd)/ansible/hosts $(pwd)/ansible/site.yml
 
-#Remove files
-#rm -f  $(pwd)/* 
+#Clean `vm-host`
+rm -f  $(pwd)/* 
